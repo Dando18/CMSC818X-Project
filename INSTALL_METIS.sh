cff58308057efcb96d98e302bab484c2c2f93175
Benjie Miao <bimiao@umd.edu>
2020-10-29 00:43:08 +0800

    update metis

--- INSTALL_METIS.sh
+++ INSTALL_METIS.sh
@@ -1,8 +1,8 @@
 mkdir -p metis
-wget -c http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz
-tar -zxvf metis-5.1.0.tar.gz 
+# wget -c http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz
+# tar -zxvf metis-5.1.0.tar.gz 
 cd metis-5.1.0
 make config prefix=$(pwd)/../metis
 make
 make install
-rm -rf metis-5.1.0.tar.gz
+# rm -rf metis-5.1.0.tar.gz
