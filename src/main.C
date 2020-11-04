bcdbaec773c835c4fe69e4e1380965618b0ab593
Daniel Nichols <dando18studios@gmail.com>
2020-11-04 17:17:34 -0500

    updates to algorithm

--- src/main.C
+++ src/main.C
@@ -11,17 +11,20 @@
 // STL Headers
 #include <iostream>
 #include <string>
+#include <queue>
 #include <ostream>
+#include <algorithm>
 
 // 3rd party libs
 #include "mpi.h"
+#include <metislib.h>
 
-/* forward declare types 
-   TODO -- this can be removed once we include METIS headers */
-struct graph_t;
+
+constexpr std::size_t MAX_COLOR = 1000;
 
 /* forward declare functions */
 graph_t *getGraph(std::string const& filename);
+graph_t *tmpGetGraph();
 void colorGraph(graph_t *graph);
 void outputColoring(graph_t *graph, std::ostream &out);
 
@@ -35,6 +38,9 @@ int main(int argc, char **argv) {
 
     /* Read in graph structure and partition */
     graph_t *graph = getGraph("");
+
+    /* a temporary getGraph function for testing */
+    graph = tmpGetGraph();
 
     /* color graph in parallel */
     double start = MPI_Wtime();
@@ -79,16 +85,113 @@ graph_t *getGraph(std::string const& filename) {
 }
 
 
+/** For testing until graph partitioning part is done.
+ *  [TODO] -- remove this!
+ *  @returns a graph_t object
+ */
+graph_t *tmpGetGraph() {
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size != 3) {
+        MPI_Abort(MPI_COMM_WORLD, 0);
+    }
+
+    graph_t *graph = new graph_t;
+    constexpr idx_t NVERTS = 5;
+    graph->nvtxs = 5;
+    graph->vwgt = new idx_t[5];
+
+
+    if (rank == 0) {
+
+        constexpr idx_t NEDGES = 14;
+        graph->nedges = NEDGES;
+        graph->adjncy = new idx_t[NEDGES] {1, 3, 0, 4, 0, 3, 4, 0, 2, 4, 1, 2, 3, 0};
+        graph->adjwgt = new idx_t[NEDGES] {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2};
+        graph->xadj = new idx_t[NVERT+1] {0, };
+        
+
+    } else if (rank == 1) {
+
+    } else if (rank == 2) {
+
+    } else if (rank == 3) {
+
+    }
+
+    return static_cast<graph_t *>(nullptr);
+}
+
+
 /** Does parallel graph coloring.
  * @param[in,out] graph the graph to be colored
  */
 void colorGraph(graph_t *graph) {
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size):
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     /* [TODO] --implement graph algorithm */
+    graph_t graph = tmpGetGraph();
+    idx_t U = //range(graph->nvtxs);
+    int max_size_U = 1;
+    while (max_size_U != 0) {
+        // [U1, ..., Un] = partition(graph, s);
+        
+    }
 
+
+
+
+}
+
+/** Does the serial graph coloring on a subgraph.
+ * Currently it's a naive breadth first search coloring method.
+ * @param[in,out] graph the graph to be colored.
+ */
+// void colorGraphSerial_BFS(idx_t *xadj, idx_t *vcolors) {
+//     std::queue<idx_t> q;
+//     q.push(0);
+
+//     bool neighbor_colors[MAX_COLOR];
+//     while (!q.empty()) {
+//         idx_t vertex = q.back();
+//         q.pop();
+//         std::fill(neighbor_colors + 0, neighbor_colors + MAX_COLOR, false);
+//         for (int i = xadj[vertex]; i < xadj[vertex + 1]; i++) {
+//             if (vcolors[i] != 0) { // not colored yet
+//                 q.push(i);
+//                 neighbor_colors[vcolors[i]] = true; // this color is used
+//             }
+//         }
+
+//         for (std::size_t i = 1; i < MAX_COLOR; i++) {
+//             if (!neighbor_colors[i]) { // first color not used
+//                 vcolors[vertex] = i;
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+void colorGraphSerial(idx_t *U, int size_U, graph_t graph) {
+    bool neighbor_colors[MAX_COLOR];
+    for (int i = 0; i < size_U; i++) {
+        idx_t vertex = U[i];
+        std::fill(neighbor_colors + 0, neighbor_colors + MAX_COLOR, false);
+        for (int j = graph->xadj[vertex]; j < graph->xadj[vertex + 1]; j++) {
+            idx_t neighbor = graph->adjncy[j];
+            neighbor_colors[graph->vwgt[neighbor]] = true; // this color is used
+        }
+        for (std::size_t j = 1; j < MAX_COLOR; j++) {
+            if (!neighbor_colors[j]) { // first color not used
+                vcolors[vertex] = j;
+                break;
+            }
+        }
+    }
 }
 
 /** Outputs the coloring info of a graph into a stream.
