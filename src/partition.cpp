6d83fb8093475a88cad224e37949c5c26fe8c76b
Benjie Miao <bimiao@umd.edu>
2020-11-15 19:09:07 +0800

    complete reading and partition function in a seperate context;

--- src/partition.cpp
+++ src/partition.cpp
@@ -1,10 +1,11 @@
-
-// #include <metis.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include "metisbin.h"
 #include <vector>
+#include <map>
+#include "mpi.h"
+// #include <metis.h>
 // #include <metislib.h>
 
 void simpleReadGraph(std::string filename, graph_t *graph)
@@ -127,7 +128,46 @@ void setParams(params_t *params, graph_t *graph)
         params->iptype = METIS_IPTYPE_RANDOM;
     }
   } 
+}
 
+void prepareForPartition(idx_t options[], params_t *params, int start_index=0)
+{
+    METIS_SetDefaultOptions(options);
+    options[METIS_OPTION_OBJTYPE] = params->objtype;
+    options[METIS_OPTION_CTYPE]   = params->ctype;
+    options[METIS_OPTION_IPTYPE]  = params->iptype;
+    options[METIS_OPTION_RTYPE]   = params->rtype;
+    options[METIS_OPTION_NO2HOP]  = params->no2hop;
+    options[METIS_OPTION_MINCONN] = params->minconn;
+    options[METIS_OPTION_CONTIG]  = params->contig;
+    options[METIS_OPTION_SEED]    = params->seed;
+    options[METIS_OPTION_NITER]   = params->niter;
+    options[METIS_OPTION_NCUTS]   = params->ncuts;
+    options[METIS_OPTION_UFACTOR] = params->ufactor;
+    options[METIS_OPTION_DBGLVL]  = params->dbglvl;
+    options[METIS_OPTION_NUMBERING]  = start_index;
+}
+
+void printGraph(graph_t *graph){
+    std::cout << "N=" << graph->nvtxs << " m=" << graph->nedges << std::endl; 
+    std::cout << "xadj:";
+    for (int n=0; n<graph->nvtxs; n++) 
+        std::cout << graph->xadj[n] << " ";
+    std::cout << std::endl;
+
+    std::cout << "adjncy:";
+    for (int n=0; n<graph->nedges; n++) 
+        std::cout << graph->adjncy[n] << " ";
+    std::cout << std::endl;
+
+    std::cout << "adjwgt:";
+    for (int n=0; n<graph->nedges; n++) 
+        std::cout << graph->adjwgt[n] << " ";
+    std::cout << std::endl;
+}
+
+void cleanGraph(graph_t *graph){
+    // (TODO)
 }
 
 /*************************************************************************/
@@ -194,58 +234,145 @@ void GPPrintInfo(params_t *params, graph_t *graph)
   }
 }
 
-
 int main(int argc, char *argv[])
 {
-    graph_t *graph = new graph_t;
-    params_t *params = new params_t ;
-    idx_t options[METIS_NOPTIONS];
-    idx_t *part;
-    idx_t objval;
+    int ierr;
+    ierr = MPI_Init(&argc, &argv);
+    
+    int nprocs, rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    int status = 0;
-    simpleReadGraph(argv[1], graph);
+    graph_t *graph_local = new graph_t;
+    graph_t *graph;
+    if (rank == 0){
+        graph = new graph_t;
+        params_t *params = new params_t ;
 
+        simpleReadGraph(argv[1], graph);
 
-    part = new idx_t[graph->nvtxs];
+        setParams(params, graph);
+        GPPrintInfo(params, graph);
 
-    setParams(params, graph);
+        int start_index = 1;
+        idx_t options[METIS_NOPTIONS];
+        prepareForPartition(options, params, start_index);
+        params->nparts = nprocs;
 
-    METIS_SetDefaultOptions(options);
-    options[METIS_OPTION_OBJTYPE] = params->objtype;
-    options[METIS_OPTION_CTYPE]   = params->ctype;
-    options[METIS_OPTION_IPTYPE]  = params->iptype;
-    options[METIS_OPTION_RTYPE]   = params->rtype;
-    options[METIS_OPTION_NO2HOP]  = params->no2hop;
-    options[METIS_OPTION_MINCONN] = params->minconn;
-    options[METIS_OPTION_CONTIG]  = params->contig;
-    options[METIS_OPTION_SEED]    = params->seed;
-    options[METIS_OPTION_NITER]   = params->niter;
-    options[METIS_OPTION_NCUTS]   = params->ncuts;
-    options[METIS_OPTION_UFACTOR] = params->ufactor;
-    options[METIS_OPTION_DBGLVL]  = params->dbglvl;
-    options[METIS_OPTION_NUMBERING ]  = 1;
-    GPPrintInfo(params, graph);
+        idx_t objval;
+        idx_t *part = new idx_t[graph->nvtxs];
+        int status = 0;
+        status = METIS_PartGraphKway(&graph->nvtxs, &graph->ncon, graph->xadj, 
+                graph->adjncy, graph->vwgt, graph->vsize, graph->adjwgt, 
+                &params->nparts, params->tpwgts, params->ubvec, options, 
+                &objval, part);
+        for (idx_t i=0; i<graph->nvtxs; i++){
+            part[i] -= start_index;
+            // std::cout << "Node:" << i << "\tPart:" << part[i] << std::endl; 
+        }
+    
+        // broadcast
+        // set up a mapping from global_idx to (rank, local_idx)
+        std::map<idx_t, std::pair<idx_t, idx_t> > g_to_l;
+        std::vector<idx_t> local_num(nprocs, 0);
+        for (idx_t i=0; i<graph->nvtxs; i++) {
+            g_to_l[i] = std::make_pair(part[i], local_num[part[i]]++);
+        }
+        // for (auto x:g_to_l) {
+        //     std::cout << x.first << " (" << x.second.first 
+        //         << ", " << x.second.second << ")" << std::endl; 
+        // }   
 
-//   switch (params->ptype) {
-    // case METIS_PTYPE_RB:
-    // status = METIS_PartGraphRecursive(&graph->nvtxs, &graph->ncon, graph->xadj, 
-    //             graph->adjncy, graph->vwgt, graph->vsize, graph->adjwgt, 
-    //             &params->nparts, params->tpwgts, params->ubvec, options, 
-    //             &objval, part);
-    //   break;
+        graph_t **graph_local_all = new graph_t* [nprocs];
+        std::vector<std::vector<idx_t>> adjncy_all(nprocs);
+        std::vector<std::vector<idx_t>> adjwgt_all(nprocs);
+        std::vector<std::vector<idx_t>> xadj_all(nprocs, std::vector<idx_t>(1,0));
 
-    // case METIS_PTYPE_KWAY:
-      status = METIS_PartGraphKway(&graph->nvtxs, &graph->ncon, graph->xadj, 
-                   graph->adjncy, graph->vwgt, graph->vsize, graph->adjwgt, 
-                   &params->nparts, params->tpwgts, params->ubvec, options, 
-                   &objval, part);
-    //   break;
-    for (idx_t i=0; i<graph->nvtxs; i++){
-        std::cout << "Node:" << i << "\tPart:" << part[i] << std::endl; 
+        for (int proc=0; proc<nprocs; proc++)
+            graph_local_all[proc]->nvtxs = local_num[proc];
+        for (int i=0; i<graph->nvtxs; i++)
+        {
+            idx_t lrank = g_to_l[i].first;
+            for (int edg_idx=graph->xadj[i]; 
+                     edg_idx<graph->xadj[i+1];
+                     edg_idx++)
+            {
+                idx_t edg = graph->adjncy[edg_idx] - start_index;
+                // std::cout << "Edge: " << edg << "local:(" <<g_to_l[edg].first << "," 
+                //     << g_to_l[edg].second << ")" <<  std::endl;
+                adjncy_all[lrank].push_back(g_to_l[edg].second);
+                adjwgt_all[lrank].push_back(g_to_l[edg].first);
+            }
+            xadj_all[lrank].push_back(adjncy_all[lrank].size());
+        }
+        for (int proc=0; proc<nprocs; proc++)
+            graph_local_all[proc]->nedges = adjncy_all[proc].size();
+
+        for (int proc=0; proc<nprocs; proc++) { 
+            graph_t *now_graph = graph_local_all[proc];
+            now_graph->adjncy = new idx_t [adjncy_all[proc].size()];
+            std::copy(adjncy_all[proc].begin(), adjncy_all[proc].end(), now_graph->adjncy);
+            now_graph->adjwgt = new idx_t [adjwgt_all[proc].size()];
+            std::copy(adjwgt_all[proc].begin(), adjwgt_all[proc].end(), now_graph->adjwgt);
+            now_graph->xadj = new idx_t [xadj_all[proc].size()];
+            std::copy(xadj_all[proc].begin(), xadj_all[proc].end(), now_graph->xadj);
+        }
+
+        for (int proc=0; proc<nprocs; proc++) {
+            std::cout << "=========================" << std::endl; 
+            std::cout << "Print Graph in process " << proc << std::endl;
+            printGraph(graph_local_all[proc]);
+            std::cout << "=========================" << std::endl; 
+        }
+
+        // Now finally we are transporting the graph
+        for (int proc=0; proc<nprocs; proc++){
+            graph_t *now_graph = graph_local_all[proc];
+            if (proc == 0) {
+                memcpy(graph_local, now_graph, sizeof(graph));
+                graph_local->xadj = new idx_t[graph_local->nvtxs];
+                memcpy(graph_local->xadj, now_graph->xadj, sizeof(idx_t) * graph_local->nvtxs);
+            
+                graph_local->adjncy = new idx_t[graph_local->nedges];
+                memcpy(graph_local->adjncy, now_graph->adjncy, sizeof(idx_t) * graph_local->nedges);
+                graph_local->adjwgt = new idx_t[graph_local->nedges];
+                memcpy(graph_local->adjwgt, now_graph->adjwgt, sizeof(idx_t) * graph_local->nedges);
+            } else {
+                MPI_Datatype datatype;
+                if (sizeof(idx_t) == 4) datatype = MPI_INT;
+                else if (sizeof(idx_t) == 8) datatype = MPI_LONG;
+                else datatype = MPI_INT;
+                MPI_Send(now_graph, sizeof(graph_t), MPI_CHAR, 0, 49, MPI_COMM_WORLD);
+                MPI_Send(now_graph->xadj, now_graph->nvtxs, datatype, 0, 50, MPI_COMM_WORLD);
+                MPI_Send(now_graph->adjncy, now_graph->nedges, datatype, 0, 51, MPI_COMM_WORLD);
+                MPI_Send(now_graph->adjwgt, now_graph->nedges, datatype, 0, 52, MPI_COMM_WORLD);
+            }
+
+            
+        }
+
+        // clean upp
+        cleanGraph(graph);
+        for (int proc=0; proc<nprocs; proc++) 
+            cleanGraph(graph_local_all[proc]);
+        delete [] part; 
+        delete params;
     }
-    delete [] part; 
-    // clean_graph(graph);
+    else {
+        MPI_Recv(graph_local, sizeof(graph_t), MPI_CHAR, 0, 49, MPI_COMM_WORLD, nullptr);
+        MPI_Datatype datatype;
+        if (sizeof(idx_t) == 4) datatype = MPI_INT;
+        else if (sizeof(idx_t) == 8) datatype = MPI_LONG;
+        else datatype = MPI_INT;
+        graph_local->xadj = new idx_t[graph_local->nvtxs];
+        MPI_Recv(graph_local->xadj, graph_local->nvtxs, datatype, 0, 50, MPI_COMM_WORLD, nullptr);
+        graph_local->adjncy = new idx_t[graph_local->nedges];
+        graph_local->adjwgt = new idx_t[graph_local->nedges];
+        MPI_Recv(graph_local->adjncy, graph_local->nedges, datatype, 0, 51, MPI_COMM_WORLD, nullptr);
+        MPI_Recv(graph_local->adjwgt, graph_local->nedges, datatype, 0, 52, MPI_COMM_WORLD, nullptr);
+    }
+
+    MPI_Finalize();
     return 0;
-  }
+}
 
